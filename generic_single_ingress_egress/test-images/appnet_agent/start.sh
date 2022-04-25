318b3843ed807922cf0833fc0954c01666d77e62
Angel Velazquez <angelcar@amazon.com>
2022-04-24 20:26:06 -0700

    Configure app and remote ports for envoy

--- generic_single_ingress_egress/test-images/appnet_agent/start.sh
+++ generic_single_ingress_egress/test-images/appnet_agent/start.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 
-ingress_port=$(echo ${SC_CONFIG} | jq -c '.ingressConfig | .[] | select (.listenerName == "ingress_listener") | .ListenerPort ')
+ingress_port=$(echo ${SC_CONFIG} | jq -c '.ingressConfig | .[] | select (.listenerName == "ingress_listener") | .listenerPort ')
 
 if [ -z "$ingress_port" ]
 then
-  ingress_port=$(echo ${listener_port_mapping} | jq -c '.[] | select(.ingress_listener) | .ingress_listener')
+  ingress_port=$(echo ${listener_port_mapping} | jq -c '.ingress_listener')
 fi
 
 
-egress_port=$(echo ${listener_port_mapping} | jq -c '.[] | select(.egress_listener) | .egress_listener')
+egress_port=$(echo ${listener_port_mapping} | jq -c '.egress_listener')
+
+app_port=$(echo ${SC_CONFIG} | jq -c '.ingressConfig | .[] | select (.listenerName == "ingress_listener") | .interceptPort ')
+if [ -z "$app_port" ]
+then
+  app_port=${SC_APP_PORT}
+fi
 
 _SC_INGRESS_PORT_=${ingress_port:-15000}
 _SC_EGRESS_PORT_=${egress_port:-30000}
+_SC_APP_PORT_=${app_port:-8080}
+_SC_REMOTE_PORT_=${SC_REMOTE_PORT:-7575}
 
 sed -i "s/SC_INGRESS_PORT/${_SC_INGRESS_PORT_}/g" "/etc/envoy/lds_config.yaml"
 sed -i "s/SC_EGRESS_PORT/${_SC_EGRESS_PORT_}/g" "/etc/envoy/lds_config.yaml"
+sed -i "s/SC_APP_PORT/${_SC_APP_PORT_}/g" "/etc/envoy/local_eds.yaml"
+sed -i "s/SC_REMOTE_PORT/${_SC_REMOTE_PORT_}/g" "/etc/envoy/remote_eds.yaml"
+
 
 /usr/bin/envoy -c /etc/envoy/envoy.yaml -l debug
